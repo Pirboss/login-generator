e370d88dba2a3410ccbe9401db6b07c3cdc5d281
pierre <pierrejeanmougin@free.fr>
2016-04-15 14:47:12 +0200

    Commit pour la couverture 100% pour service

---
+++ src/test/java/dcll/LoginServiceTest.java
@@ -0,0 +1,37 @@
+package dcll;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by pierre on 15/04/16.
+ */
+public class LoginServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testLoginExists() throws Exception {
+
+    }
+
+    @Test
+    public void testAddLogin() throws Exception {
+
+    }
+
+    @Test
+    public void testFindAllLoginsStartingWith() throws Exception {
+
+    }
+
+    @Test
+    public void testFindAllLogins() throws Exception {
+
+    }
+}
